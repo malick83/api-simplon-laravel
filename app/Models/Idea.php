e03d753ed554494ac6a2c3c64cd7c6d8c0b0fe55
malick83 <malickkebe83@gmail.com>
2022-03-24 13:26:55 +0000

    ajout du champs statut

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Idea extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'suggestion'];
+    protected $fillable = ['title', 'suggestion', 'statut'];
 }
